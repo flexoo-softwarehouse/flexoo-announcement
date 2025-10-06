65e4bf448a026498d60930c8122368e625f33867
sadamlrfs <sadamalirafsanjani365@gmail.com>
2025-10-06 21:07:07 +0700

    fix: fix code check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export default function Home() {
-  const router = useRouter();
-
-  // useEffect(() => {
-  //   // Redirect otomatis ke /announcement
-  //   router.replace("/announcement");
-  // }, [router]);
-
-  // Tampilkan loading atau tidak ada yang ditampilkan
   return null;
 }
